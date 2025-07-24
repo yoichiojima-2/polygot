bedd45b8170561aaeffbb387b779d663ee7cdb31
yoichi ojima <yoichiojima@gmail.com>
2025-07-24 20:48:10 +0900

    refactor: follow c good practice

--- c/main.c
+++ c/main.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdio.h>
+#include "functions.h"
 
 int main() {
   char buffer[BUFFER_SIZE];
--- c/test_main.c
+++ c/test_main.c
@@ -1,12 +1,15 @@
-#include "main.h"
+#include "functions.h"
+#include <stdio.h>
 #include <string.h>
 
 void test_create_health_response() {
-  char buffer[BUFFER_SIZE];
-  create_health_response(buffer);
-  if (strcmp(buffer, "{\"language\": \"c\", \"version\": \"0.1.0\"}") == 0) {
-    printf("test_create_health_response: PASSED\n");
-  }
+    char buffer[BUFFER_SIZE];
+    create_health_response(buffer);
+    if (strcmp(buffer, "{\"language\": \"c\", \"version\": \"0.1.0\"}") == 0) {
+        printf("test_create_health_response: PASSED\n");
+    } else {
+        printf("test_create_health_response: FAILED\n");
+    }
 }
 
 int main() {
