7f601249e36f16fb600fc2d1b42d8fdc73d34885
yoichi ojima <yoichiojima@gmail.com>
2025-07-24 20:13:13 +0900

    style: apply formatter

--- c/main.c
+++ c/main.c
@@ -4,20 +4,16 @@
 #define BUFFER_SIZE 1000
 
 // create json response
-void create_health_response(char* buffer) {
-    snprintf(
-        buffer,
-        BUFFER_SIZE,
-        "{\"language\": \"c\", \"version\": \"%s\"}",
-        VERSION
-    );
+void create_health_response(char *buffer) {
+  snprintf(buffer, BUFFER_SIZE, "{\"language\": \"c\", \"version\": \"%s\"}",
+           VERSION);
 }
 
 int main() {
-    char buffer[BUFFER_SIZE];
+  char buffer[BUFFER_SIZE];
 
-    create_health_response(buffer);
-    printf("health response: %s\n", buffer);
+  create_health_response(buffer);
+  printf("health response: %s\n", buffer);
 
-    return 0;
+  return 0;
 }
