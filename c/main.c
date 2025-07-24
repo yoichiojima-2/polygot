6fbe81db0ffdcd7c2c18595f8418a6081403769b
yoichi ojima <yoichiojima@gmail.com>
2025-07-24 20:31:30 +0900

    test: add test_create_health_check

--- c/main.c
+++ c/main.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-
-#define VERSION "0.1.0"
-#define BUFFER_SIZE 1000
-
-// create json response
-void create_health_response(char *buffer) {
-  snprintf(buffer, BUFFER_SIZE, "{\"language\": \"c\", \"version\": \"%s\"}",
-           VERSION);
-}
+#include "main.h"
 
 int main() {
   char buffer[BUFFER_SIZE];
---
+++ c/test_main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+void test_create_health_response() {
+  char buffer[BUFFER_SIZE];
+  create_health_response(buffer);
+  if (strcmp(buffer, "{\"language\": \"c\", \"version\": \"0.1.0\"}") == 0) {
+    printf("test_create_health_response: PASSED\n");
+  }
+}
+
+int main() {
+  char buffer[BUFFER_SIZE];
+  test_create_health_response();
+  return 0;
+}
