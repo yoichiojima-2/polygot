4db038add3e8cba65e26b4aa359ffb5a0a7edfa8
yoichi ojima <yoichiojima@gmail.com>
2025-07-30 16:20:01 +0900

    feat: add server with ts

---
+++ ts/main.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+
+const PORT: string = "8080";
+const VERSION: string = "1.0.0";
+
+interface HealthResponse {
+  language: string;
+  version: string;
+}
+
+const health = (req: Request, res: Response): void => {
+  const response: HealthResponse = {
+    language: "ts",
+    version: VERSION ,
+  };
+  res.status(200).json(response);
+}
+
+function main() {
+  const app = express();
+  app.get("/health", health);
+
+  app.listen(PORT, (): void => {
+    console.log(`server running on port ${PORT}`)
+  });
+}
+
+main();
